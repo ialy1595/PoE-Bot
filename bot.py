49ad28a9e7450ab294eedbf888e2b5b86a7465ab
ialy1595 <ialy12006@gmail.com>
2020-09-23 08:06:05 -0400

    implement searching wiki

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-def find_trade(query, is_simple):
+def find_trade(driver, query, is_simple):
     url_query = query.strip().replace(" ","%20")
 
     if len(url_query) == 0:
@@ -56,13 +56,6 @@ def find_trade(query, is_simple):
     }
 
     res = []
-
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
-    
-    driver = webdriver.Chrome('driver-linux/chromedriver', chrome_options=options)
 
     target_cat = simple_categories if is_simple else categories
 
@@ -116,8 +109,82 @@ def find_trade(query, is_simple):
         
     return res
 
+def find_wiki(driver, query):
+    url = 'https://pathofexile.gamepedia.com/index.php?search=intitle%3A%2F{}%2F'.format(query)
+
+    driver.get(url)
+
+    WebDriverWait(driver, 2).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'searchresults'))
+    )
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    sr = soup.find_all('div', class_='searchresults')
+    no_result = sr[0].find_all('p', class_='mw-search-nonefound')
+
+    if len(no_result) > 0:
+        return []
+    
+    res = []
+
+    result_boxes = sr[0].find_all('li', class_='mw-search-result')
+
+    top_results = result_boxes if len(result_boxes) <= 5 else result_boxes[:5]
+
+    for r in top_results:
+        content = r.find_all('div')[0]
+        title = content.get_text()
+        link = "https://pathofexile.gamepedia.com" + content.find_all('a')[0].get('href')
+        res.append({
+            'title': title,
+            'link': link
+        })
+    
+    return res
+
+def find_wiki_korean(driver, query):
+    url = 'https://poedb.tw/kr/search.php?q={}'.format(query)
+
+    driver.get(url)
+
+    WebDriverWait(driver, 2).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'form-search'))
+    )
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    page = soup.find_all('div', class_='page')
+
+    res = []
+
+    result_boxes = page[0].find_all('li', class_='list-group-item')
+
+    top_results = result_boxes if len(result_boxes) <= 5 else result_boxes[:5]
+
+    for r in top_results:
+        content = r.find_all('a')[0]
+        title = content.get_text()
+        link = "https://poedb.tw/" + content.get('href')
+        res.append({
+            'title': title,
+            'link': link
+        })
+
+    return res
 
 if __name__ == "__main__":
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    
+    driver = webdriver.Chrome('driver-linux/chromedriver', chrome_options=options)
+
+
     t = open('token.txt', 'r')
     token = t.read().rstrip()
     blank = '\u200B'
@@ -141,7 +208,7 @@ if __name__ == "__main__":
 
         if message.content.startswith('!price') or message.content.startswith('!가격') or message.content.startswith('!detail_price') or message.content.startswith('!상세가격'):
 
-            await message.channel.send("검색중...")
+            await message.channel.send("가격 검색중...")
 
             query = ""
             is_simple = True
@@ -157,7 +224,7 @@ if __name__ == "__main__":
                 query = message.content[6:]
                 is_simple = False
 
-            result = find_trade(query, is_simple)
+            result = find_trade(driver, query, is_simple)
 
             embed = discord.Embed(title = (query + ' 가격 검색 결과'))
             embed.add_field(name = '이름', value = blank, inline = True)
@@ -179,5 +246,47 @@ if __name__ == "__main__":
                     embed.add_field(name = blank, value = r['price'], inline = True)
 
             await message.channel.send(embed = embed)
+        
+        if message.content.startswith('!wiki'):
+            await message.channel.send("wiki searching...")
+            query = message.content[6:]
 
+            result = find_wiki(driver, query)
+
+            embed_title = query + ' wiki search result'
+
+            if len(result) == 5:
+                embed_title += ' (top 5)'
+
+            embed = discord.Embed(title = embed_title)
+
+            if len(result) == 0:
+                embed.add_field(name = 'There is', value = "no result", inline = False)
+                await message.channel.send(embed = embed)
+            else:
+                for r in result:
+                    embed.add_field(name = r['title'], value = r['link'], inline = False)
+                await message.channel.send(embed = embed)
+
+        if message.content.startswith('!위키'):
+            await message.channel.send("위키 검색중...")
+            query = message.content[4:]
+
+            result = find_wiki_korean(driver, query)
+
+            embed_title = query + ' 위키 검색 결과'
+
+            if len(result) == 5:
+                embed_title += ' (top 5)'
+
+            embed = discord.Embed(title = embed_title)
+
+            if len(result) == 0:
+                embed.add_field(name = '검색 결과가', value = "없습니다", inline = False)
+                await message.channel.send(embed = embed)
+            else:
+                for r in result:
+                    embed.add_field(name = r['title'], value = r['link'], inline = False)
+                await message.channel.send(embed = embed)
+                
     client.run(token)
